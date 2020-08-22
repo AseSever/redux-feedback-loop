e780cf893a95f4fcd3b436da4031e2a6b74ea334
Alex Severson <alexsvrsn@gmail.com>
2020-08-21 23:17:55 -0500

    imported redux, express, created feeling component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Feeling from '../Feeling/Feeling.jsx'
 
 class App extends Component {
   render() {
@@ -10,6 +12,10 @@ class App extends Component {
           <h1 className="App-title">Feedback!</h1>
           <h4><i>Don't forget it!</i></h4>
         </header>
+
+        <Router>
+          <Route exact path='/' component={Feeling} />
+        </Router>
         <br/>
       </div>
     );
